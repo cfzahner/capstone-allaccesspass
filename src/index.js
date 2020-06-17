9dc6b3d064b9d61a48cdb1307306ac87c3703c44
Courtney <cfzahner@gmail.com>
2020-06-17 18:14:39 -0500

    Talk to react-router dom with class App. State rendering from the root.

--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ import Header from "./components/header";
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
-);
+import { BrowserRouter as Router, Route } from "react-router-dom";
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Header />
+        <Route exact path="/" component={login} />
+        <Route path="/signup" component={signup} />
+        <Route path="/home" component={home} />
+      </Router>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
+ReactDOM.render(<App />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
