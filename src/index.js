5e4f5ad09c57a3f6e7bd5a184c238e66117806cb
Manav Misra <manavm@visionwebsoft.com>
2020-06-18 16:44:54 -0500

    Reset app to 'initial' hello world

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import { App } from "./App";
 
-import Header from "./components/header";
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-import { BrowserRouter as Router, Route } from "react-router-dom";
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Header />
-        <Route exact path="/" component={login} />
-        <Route path="/signup" component={signup} />
-        <Route path="/home" component={home} />
-      </Router>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  document.getElementById("root")
+);
