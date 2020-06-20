48764bbcf4a8de294611d92e7f8be4657d952567
Courtney <cfzahner@gmail.com>
2020-06-20 12:16:29 -0500

    Build button with proptype

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,3 +1,17 @@
 import React from "react";
 
-export const Button = () => <button>Click Me!</button>;
+export const Button = ({ buttonClass, label, type }) => (
+  <button type={type} className={buttonClass}>
+    {label}
+  </button>
+);
+Button.defaultProps = {
+  buttonClass: "success",
+  label: "Submit",
+  type: "submit",
+};
+Button.propTypes = {
+  buttonClass: PropTypes.string,
+  label: PropTypes.string,
+  type: PropTypes.string,
+};
