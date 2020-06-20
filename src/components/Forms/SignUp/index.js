5c98b5bf776ecb672f0e1980195496940413a865
Courtney <cfzahner@gmail.com>
2020-06-20 16:08:35 -0500

    Set array for inputs.

--- src/components/Forms/SignUp/index.js
+++ src/components/Forms/SignUp/index.js
@@ -8,51 +8,33 @@ export class SignUp extends React.Component {
     password: "",
   };
 
-  handleChange(e) {
-    this.setState({ [e.target.title]: e.target.value });
-  }
+  inputs = [
+    {
+      inputType: "text",
+      labelText: "Name",
+    },
+    {
+      inputType: "email",
+      labelText: "Email",
+    },
+    {
+      inputType: "text",
+      labelText: "User Name khjkhjk",
+    },
+    {
+      inputType: "password",
+      labelText: "password",
+    },
+  ];
 
   handleSubmit(event) {
-    alert("A name was submitted:" + this.state.value);
+    alert("User submitted:" + this.state.value);
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <input
-            type="text"
-            title="Name"
-            placeholder="Name"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-
-          <input
-            type="text"
-            title="Email"
-            placeholder="Email"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            title="Username"
-            placeholder="Username"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-          <input
-            type="password"
-            title="Password"
-            placeholder="Password"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <form onSubmit={this.handleSubmit}>{this.renderInputs(this.inputs)}</form>
     );
   }
 }
