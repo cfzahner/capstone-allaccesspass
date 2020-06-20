a10f6762d851c536dca2ce1633b897735884e217
Courtney <cfzahner@gmail.com>
2020-06-20 16:53:46 -0500

    Set state conditionally to target value.

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -5,8 +5,24 @@ import Input from "./Input";
 export class Form extends React.Component {
   handleChange = (event) => {
     //dot notation(interpolation)
-    this.setState({ [event.target.value]: "value" });
+    this.setState({
+      [event.target.dataset.st]:
+        event.target.type === "checkbox"
+          ? event.target.checked
+          : event.target.value,
+    });
   };
+
+  processFormData = (formControls, datasetKey) =>
+    // Convert to ARRAY and 'filter' only inputs with 'ids' (not 'button', etc.)
+    Array.from(formControls)
+      .filter(({ dataset }) => dataset[datasetKey])
+      // 'map' and transform each 'input' into an OBJECT
+      .map(({ dataset, value }) => ({ [dataset[datasetKey]]: value }))
+      .reduce((accumulatedData, currentData) => ({
+        ...accumulatedData,
+        ...currentData,
+      }));
 
   renderInputs = (inputs) =>
     // Each type of form will have an Array of Objects with the desired input props
