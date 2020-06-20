1bdf233f8898da78988a2da0d786426e3a13d6c2
Courtney <cfzahner@gmail.com>
2020-06-20 12:24:05 -0500

    Add import for proptype and button.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import PropTypes from "prop-types";
+import "./Button.css";
 
 export const Button = ({ buttonClass, label, type }) => (
   <button type={type} className={buttonClass}>
