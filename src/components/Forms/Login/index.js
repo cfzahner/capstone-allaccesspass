35c7863b9a7aa25e685056c4e74395eeaa1ea4dd
Courtney <cfzahner@gmail.com>
2020-06-20 17:21:36 -0500

    Add input objects.

--- src/components/Forms/Login/index.js
+++ src/components/Forms/Login/index.js
@@ -4,60 +4,36 @@ import { Button } from "components/Button";
 import styles from "./login.css";
 export class Login extends Form {
   state = {
-  constructor(props) {
-    super(props);
-    this.state = {
     loggedIn: false,
     name: "",
     username: "",
     password: "",
     err: false,
   };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleChange(e) {
-    this.setState({ [e.target.title]: e.target.value });
-  }
+
+  inputs = [
+    {
+      inputType: "text",
+      labelText: "Username",
+    },
+    {
+      inputType: "password",
+      labelText: "Password",
+    },
+  ];
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
-    const { username, password } = this.state;
-    if (username === "test" && password === "test") {
-      this.setState({ loggedIn: "true" });
-      localStorage.setItem("loggedin", true);
-    } else {
-      this.setState({ err: "Error, please check username and password" });
-    }
+    console.log(this.processFormData(e.target, "st"));
   };
 
   render() {
-    return localStorage.getItem("loggedin") === "true" ? (
-      <div className="main-login">
-        <form>
-          <input
-            type="text"
-            title="username"
-            placeholder="username"
-            onChange={this.handleChange}
-          />
-          <input
-            type="password"
-            title="password"
-            placeholder="password"
-            onChange={this.handleChange}
-          />
-          <button type="submit" onClick={this.handleSubmit}>
-            submit
-          </button>
-        </form>
-        <p>
-          Or Create <a href="/signup">Account</a>
-        </p>
-      </div>
-    ) : (
-      <div>logged in</div>
+    return (
+      <form className={styles.form}>
+        {this.renderInputs(this.inputs)}
+        <Button />
+        <Button buttonClass="plain" label="Register?" type="button" />
+      </form>
     );
   }
 }
