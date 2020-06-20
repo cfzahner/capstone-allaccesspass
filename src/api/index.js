0be91165dd296ceaa750d14ee280ece1be24c51a
Courtney <cfzahner@gmail.com>
2020-06-20 17:07:10 -0500

    Add async to post response for users.

--- src/api/index.js
+++ src/api/index.js
@@ -2,3 +2,12 @@ export const getAllCandidates = async () => {
   const resp = await fetch("http://localhost:1000/api/candidates/candidates");
   return resp.json();
 };
+export const authenticateUser = async (formData) => {
+  const resp = await fetch("", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: JSON.stringify(formData),
+  });
+};
