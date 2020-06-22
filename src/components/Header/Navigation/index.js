fe529d7f6a2cc7df8d37c0f48c391d9f2d143a57
Courtney <cfzahner@gmail.com>
2020-06-22 16:11:01 -0500

    Import nav link to map and return requested link.

--- src/components/Header/Navigation/index.js
+++ src/components/Header/Navigation/index.js
@@ -1,30 +1,51 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import "./navigation.css";
+const links = ["Home", "About", "Events", "Reviews"];
 
-export class Navigation extends React.Component {
-  render() {
+const createLinks = () => {
+  return links.map((link) => {
     return (
-      <nav className="Nav">
-        <ul>
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-
-          <Link to="/about">
-            <li>About Us</li>
-          </Link>
-
-          <Link to="/events">
-            <li>Events</li>
-          </Link>
-
-          <Link to="/reviews">
-            <li>Reviews</li>
-          </Link>
-        </ul>
-      </nav>
+      <li key={link}>
+        <NavLink to={"/" + link.toLowerCase()} activeClassName="isActiveLink">
+          {link}
+        </NavLink>
+      </li>
     );
-  }
-}
+  });
+};
+
+export const Navigation = () => (
+  <nav>
+    <ul>{createLinks()}</ul>
+  </nav>
+);
+
+//export class Navigation extends React.Component {
+//  render() {
+//    return (
+//      <nav className="Nav">
+//        <ul>
+//          <Link to="/">
+//            <li>Home</li>
+//          </Link>
+//
+//          <Link to="/about">
+//            <li>About Us</li>
+//          </Link>
+//
+//          <Link to="/events">
+//            <li>Events</li>
+//          </Link>
+//
+//          <Link to="/reviews">
+//            <li>Reviews</li>
+//          </Link>
+//        </ul>
+//      </nav>
+//    );
+//  }
+//}
+//const createLinks = () => {
+//  return links.map((link) => {
