53c9c95a7319b428879cbac25b9f46069f731f3e
Courtney <cfzahner@gmail.com>
2020-06-22 16:02:54 -0500

    Add require config for dotenv.

--- backend/db/client.js
+++ backend/db/client.js
@@ -1,8 +1,9 @@
 import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 
-dotenv.config();
-
+//dotenv.config();
+require("dotenv").config();
+console.log(process.env.ATLAS_URI);
 export const client = new MongoClient(process.env.ATLAS_URI, {
   useUnifiedTopology: true,
 });
