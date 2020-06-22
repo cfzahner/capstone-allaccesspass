382b4c25ec50d581aa7d4b5c0c9349c4efc562e6
Courtney <cfzahner@gmail.com>
2020-06-22 16:13:59 -0500

    Change styles to module and adjust class names accordingly.

--- src/components/Pages/About/index.js
+++ src/components/Pages/About/index.js
@@ -1,22 +1,23 @@
 import React from "react";
+import styles from "./about.module.css";
 
-export class AboutUs extends React.Component {
+export class About extends React.Component {
   render() {
     return (
-      <div>
+      <div className={styles.mainAbout}>
         <main>
-          <h1>About Us!</h1>
-          <section>
-            <h2>Who we work with?</h2>
-            <p>
+          <h1 className={styles.title}>About Us!</h1>
+          <section className={styles.body}>
+            <h2 className={styles.companies}>Who we work with?</h2>
+            <p className={styles.p.body}>
               Here's a list of companies and organizations that we partner with
               for job placement and furthuring education.
             </p>
           </section>
 
           <section>
-            <h2>Why we are here for you?</h2>
-            <p>
+            <h2 className={styles.title2}>Why we are here for you?</h2>
+            <p className={styles.message}>
               We're all in this together and need to support our fellow ladies.
             </p>
           </section>
