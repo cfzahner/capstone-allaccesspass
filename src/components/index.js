6211a72737c8c47e72785410f3545522465980ff
Courtney <cfzahner@gmail.com>
2020-06-22 10:05:40 -0500

    Add about us to export for routing ability.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { Header } from "./Header";
 export { Main } from "./Main";
 export { Login } from "./Forms/Login";
-export { Home } from "./Pages/Home";
+export { Home, AboutUs } from "./Pages";
