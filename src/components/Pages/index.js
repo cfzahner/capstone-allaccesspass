22a3fb964c8bf32bcbb165fc18799a0a2d9ed32d
Courtney <cfzahner@gmail.com>
2020-06-22 16:19:13 -0500

    Export pages for nav bar routing.

--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,2 +1,4 @@
 export { Home } from "./Home";
-export { AboutUs } from "./About";
+export { About } from "./About";
+export { Events } from "./Events/events";
+export { Reviews } from "./Reviews/reviews";
