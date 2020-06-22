c66b0fabbca39dedcf74861c5832b8df7148569b
Courtney <cfzahner@gmail.com>
2020-06-22 16:25:29 -0500

    Add routing path for components.

--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,22 @@ import { Home, About, Events, Reviews, Login } from "./components";
 
 export const Routes = () => (
   <Switch>
+    <Route exact path="/">
+      <Redirect to="/login" />
+    </Route>
     <Route exact path="/home">
       <Home />
     </Route>
-    <Route exact path="/">
-      <Redirect to="/home" />
+    <Route exact path="/about">
+      <About />
+    </Route>
+    <Route exact path="/events">
+      <Events />
+    </Route>
+    <Route exact path="/reviews">
+      <Reviews />
     </Route>
     <Route exact path="/login">
-      <Redirect to="/login" />
       <Login />
     </Route>
   </Switch>
