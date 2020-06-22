ad30a00ba95ca42e6f5f7f23bf0e5ec3b5f5ff4b
Courtney <cfzahner@gmail.com>
2020-06-22 16:15:01 -0500

    Updates for styles to be modularized and change class names.

---
+++ src/components/Pages/Events/events.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./events.module.css";
+
+export class Events extends React.Component {
+  render() {
+    return (
+      <div className={styles.back}>
+        <main>
+          <section className={styles.heroHome}>
+            <h1 className={styles.eventTitle}>Events</h1>
+
+            <h2 className={styles.h2Message}>
+              Lets talk about webinars, shadowing and education opportunities
+            </h2>
+
+            <p className={styles.events}>All the Events!</p>
+          </section>
+        </main>
+      </div>
+    );
+  }
+}
