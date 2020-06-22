c490b554439dc64a1c166a6300e2557a7a1feb30
Courtney <cfzahner@gmail.com>
2020-06-21 22:37:20 -0500

    Update imports to support style renders and provide content for section.

--- src/components/Main/Hero/index.js
+++ src/components/Main/Hero/index.js
@@ -1,3 +1,30 @@
 import React from "react";
+import { styles } from "./Hero.css";
+import "bulma/css/bulma.css";
+import { Header } from "components/Header";
+export class Hero extends React.Component {
+  // add event handler for button
 
-export const Hero = () => <p>https://bulma.io/documentation/layout/hero/</p>;
+  render() {
+    return (
+      //hero image example
+      <div className="hero-image">
+        <section>
+          <h2>Lets talk...</h2>
+
+          <p>
+            It’s a misrepresentation of our own identity, as we have a society
+            where 50% of the population are women,” says Vanessa Reed, CEO of
+            the PRS for Music Foundation, a leading music charity. “If you’re
+            underrepresented in an industry, you’re going to feel like you have
+            less reason to put yourself forward or less chance of getting
+            support. You need to see it if you’re going to be it,” she says.
+          </p>
+        </section>
+        <button className="hero-button">Create Profile</button>
+        <button className="hero-button">Browse Candidates</button>
+        <button className="hero-button">Browse Companies</button>
+      </div>
+    );
+  }
+}
