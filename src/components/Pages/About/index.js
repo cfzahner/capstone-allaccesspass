efe2a75a17a69d208a19e8c6794055d3204a7d07
Courtney <cfzahner@gmail.com>
2020-06-22 10:07:20 -0500

    Add render content with tags.

---
+++ src/components/Pages/About/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export class AboutUs extends React.Component {
+  render() {
+    return (
+      <div>
+        <main>
+          <h1>About Us!</h1>
+          <section>
+            <h2>Who we work with?</h2>
+            <p>
+              Here's a list of companies and organizations that we partner with
+              for job placement and furthuring education.
+            </p>
+          </section>
+
+          <section>
+            <h2>Why we are here for you?</h2>
+            <p>
+              We're all in this together and need to support our fellow ladies.
+            </p>
+          </section>
+        </main>
+      </div>
+    );
+  }
+}
