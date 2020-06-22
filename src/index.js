eebcc46e15f6d49c19d47a9ec93704667fe05a0e
Courtney <cfzahner@gmail.com>
2020-06-22 16:24:07 -0500

    Add react router dom import.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
 //import { App } from "./App";
 import { Routes } from "./routes";
 import { Header } from "./components/Header";
+
 import "font-awesome/css/font-awesome.min.css";
-import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <div>
+      <div className="container">
         <Header />
         <Routes />
       </div>
