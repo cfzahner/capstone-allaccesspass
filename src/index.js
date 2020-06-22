082117565826fa77d181a70c5c34d7b5e9bfacbf
Courtney <cfzahner@gmail.com>
2020-06-21 22:20:18 -0500

    Install and import react-router-dom to render routes, update render accordingly.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
-
+//import { App } from "./App";
+import { Routes } from "./routes";
+import { Header } from "./components/Header";
+import "font-awesome/css/font-awesome.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div>
+        <Header />
+        <Routes />
+      </div>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
