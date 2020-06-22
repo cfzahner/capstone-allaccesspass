a8fb018c50232cdcf75570d9e431d1f3aa13a9d7
Courtney <cfzahner@gmail.com>
2020-06-21 22:07:02 -0500

    Restructure for routing purpose.

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 import React, { Fragment } from "react";
 
 import { Login, Header, Main } from "./components";
+import { Home } from "./src/components/Pages";
 export class App extends React.Component {
   state = {
     isLoggedIn: false,
   };
 
-  function Greeting(props) {
-    const isLoggedIn = props.isLoggedIn
-    if(isLoggedIn) {
-      return <UserGreeting />
-    }
-    return <GuestGreeting />
-  }
+  //function Greeting(props) {
+  //  const isLoggedIn = props.isLoggedIn
+  //  if(isLoggedIn) {
+  //    return <UserGreeting />
+  //  }
+  //  return <GuestGreeting />
+  //}
 
   render() {
     return (
       <Fragment>
-        <Login />
+        <Home />
         <Header />
         <Main />
       </Fragment>
