d0e76be7173b3059c2eeaf74ac926030764c29d6
Courtney <cfzahner@gmail.com>
2020-06-23 17:36:11 -0500

    Export Cards

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { Header } from "./Header";
 export { Button } from "./Button";
-export { Card } from "./Card";
+export { Cards } from "./Cards";
 
 export { Login } from "./Forms/Login";
 export { Home, About, Events, Reviews } from "./pages";
