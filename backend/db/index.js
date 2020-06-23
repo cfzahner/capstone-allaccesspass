3d550ff996e6feaa13b7b6cebd6ca2aad848e779
Courtney <cfzahner@gmail.com>
2020-06-23 17:21:55 -0500

    Add try catch to talk to database to login user.

--- backend/db/index.js
+++ backend/db/index.js
@@ -14,6 +14,17 @@ export const getAllCandidates = async () => {
   }
 };
 
+export const loginUser = async (candidate) => {
+  try {
+    return await client
+      .db("candidates")
+      .collection("candidates")
+      .findOne(candidate);
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
 export const registerEmployer = async (newEmployer) => {
   try {
     return await client
