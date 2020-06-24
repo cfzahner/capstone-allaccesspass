e895a7707528e9a1aed0892129a033ba13e64063
Courtney <cfzahner@gmail.com>
2020-06-23 22:40:33 -0500

    Troubleshoot div structure.

--- src/components/Pages/About/index.js
+++ src/components/Pages/About/index.js
@@ -6,16 +6,13 @@ export class About extends React.Component {
     return (
       <div className={styles.mainAbout}>
         <main>
-          <h1 className={styles.title}>About Us!</h1>
           <section>
+            div<h1 className={styles.title}>About Us!</h1>
             <h2 className={styles.companies}>Who we work with?</h2>
-            <p className={styles.pody}>
+            <p className={styles.pBody}>
               Here's a list of companies and organizations that we partner with
               for job placement and furthuring education.
             </p>
-          </section>
-
-          <section>
             <h2 className={styles.title2}>Why we are here for you?</h2>
             <p className={styles.message}>
               We're all in this together and need to support our fellow ladies.
