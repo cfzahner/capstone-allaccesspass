74c03ab6146be0b7c1a9a37b32ba912582144861
Courtney <cfzahner@gmail.com>
2020-06-24 15:04:18 -0500

    Update import for auth  route and apply app.use

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import cors from "cors";
 import express from "express";
-
+import auth from "./routes/api/auth";
 import candidates from "./routes/api/candidates";
 import employers from "./routes/api/employers";
 
@@ -10,10 +10,11 @@ const PORT = 1000;
 app.get("/", (_, res) => res.send("<p>Howdy</p>"));
 
 app.use(cors());
+app.use(express.json());
 
 app.use("/api/candidates", candidates);
 
-app.use(express.json());
+app.use("/api/auth", auth);
 
 app.use("/api/employers", employers);
 
