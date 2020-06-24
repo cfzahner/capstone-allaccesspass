41e2b89cb5a90953aeba87edb3e01ca11780fa81
Courtney <cfzahner@gmail.com>
2020-06-24 15:11:17 -0500

    Add html for employees and employer lists

---
+++ src/components/Pages/CompanyList/companies.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class CompanyList extends React.Component {
+  render() {
+    return <div>company list</div>;
+  }
+}
---
+++ src/components/Pages/LadiesList/ladies.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class LadiesList extends React.Component {
+  render() {
+    return <div>ladies</div>;
+  }
+}
