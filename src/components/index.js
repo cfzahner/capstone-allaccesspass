da82ddd8eafc957d60eed99b21030857ca41bef3
Courtney <cfzahner@gmail.com>
2020-06-24 17:59:07 -0500

    Export  thank you page for routing.

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { Header } from "./Header";
-export { Button } from "./Button";
+//export { Button } from "./Button";
 export { Cards } from "./Cards";
 export { Login } from "./Forms/Login";
 export { Home, About, Events, Reviews, LadiesList, CompanyList } from "./pages";
 export { ChoicePage, WomanForm, CompanyForm } from "./Forms/NewProfile";
+export { ThankYou } from "./ThankYou";
