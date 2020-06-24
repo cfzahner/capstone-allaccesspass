4777a5da4bdc0593aac32cb6b3f89ec13f2c49df
Courtney <cfzahner@gmail.com>
2020-06-24 15:10:28 -0500

    Add structure for  form

---
+++ src/components/Forms/NewProfile/WomanForm/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class WomanForm extends React.Component {
+  render() {
+    return (
+      <main>
+        <section>
+          <h2>candidate form</h2>
+        </section>
+      </main>
+    );
+  }
+}
