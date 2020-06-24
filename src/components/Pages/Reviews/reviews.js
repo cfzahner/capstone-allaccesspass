8560747e414d2edcb9c8f2b78df4b11dac379581
Courtney <cfzahner@gmail.com>
2020-06-24 17:57:38 -0500

    Restructure sections for styling

--- src/components/Pages/Reviews/reviews.js
+++ src/components/Pages/Reviews/reviews.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./reviews.modules.css";
+import styles from "./reviews.module.css";
 
 export class Reviews extends React.Component {
   render() {
@@ -8,9 +8,9 @@ export class Reviews extends React.Component {
         <div className={styles.heroReview}>
           <section>
             <h1 className={styles.reviewTitle}>Reviews</h1>
-            <h2>Lets talk about reviews!</h2>
+            <h2 className={styles.headerReview}>Lets talk about reviews!</h2>
 
-            <p>
+            <p className={styles.reviews}>
               Here, you can leave a review about a good or bad experience you
               had working with a production company.
             </p>
