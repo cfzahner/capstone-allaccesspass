64e496184a73604e2da1502a6c02cb50e7670de6
Courtney <cfzahner@gmail.com>
2020-06-25 08:40:48 -0500

    Add button pages to be exported for routing.

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,12 @@ export { Header } from "./Header";
 //export { Button } from "./Button";
 export { Cards } from "./Cards";
 export { Login, ChoicePage, WomanForm, CompanyForm } from "./Forms";
-export { Home, About, Events, Reviews, Ladies } from "./pages";
+export {
+  Home,
+  About,
+  Events,
+  Reviews,
+  Ladies,
+  ThankYou,
+  Companies,
+} from "./pages";
