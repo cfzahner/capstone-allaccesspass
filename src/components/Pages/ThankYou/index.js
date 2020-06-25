f55a17d2fd6c612b0a00b5267978cf2b16799137
Courtney <cfzahner@gmail.com>
2020-06-25 08:36:16 -0500

    Add styles import add puppy pic.

--- src/components/Pages/ThankYou/index.js
+++ src/components/Pages/ThankYou/index.js
@@ -1,13 +1,18 @@
 import React from "react";
+import styles from "./ThankYou.module.css";
 
 export class ThankYou extends React.Component {
   render() {
     return (
       <div>
-        <h2>
-          High five and thanks for joining a community that aims to narrow the
-          inequality gap.
-        </h2>
+        <figure className={styles.puppy}>
+          <img
+            src={
+              "https://images.unsplash.com/photo-1534551767192-78b8dd45b51b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+            }
+            alt="Thank you for submitting"
+          />
+        </figure>
       </div>
     );
   }
