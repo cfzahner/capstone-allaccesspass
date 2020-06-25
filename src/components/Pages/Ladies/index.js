9ae3bd1f5a8ffe63639e74ab86e22b8a3676d470
Courtney <cfzahner@gmail.com>
2020-06-25 08:38:44 -0500

    Import Styles and add ladies image for placeholder.

--- src/components/Pages/Ladies/index.js
+++ src/components/Pages/Ladies/index.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "./Ladies.module.css";
 
 export class Ladies extends React.Component {
   render() {
-    return <div>ladies</div>;
+    return (
+      <div>
+        <figure className={styles.ladies}>
+          <img
+            src={
+              "https://images.unsplash.com/photo-1581404917879-53e19259fdda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80"
+            }
+            alt="Candidate List"
+          />
+        </figure>
+      </div>
+    );
   }
 }
